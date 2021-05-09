6891ec4ef93f8af321a338404bb20deb00002c4b
coderCKK <chengkkaaii@163.com>
2021-05-09 11:04:58 +0800

    第一道作业题, 题中Hello和要解码的xlass文件的类名相同, 这里使用FirstJob_Hello名称

---
+++ firstJob/src/FirstJob_Hello.java
@@ -0,0 +1,9 @@
+public class FirstJob_Hello {
+    public static void main(String[] args) {
+        int i = 1;
+        for (int j = 0; j < 5; j++) {
+            if(j % 2==0)
+                i *= 2;
+        }
+    }
+}
