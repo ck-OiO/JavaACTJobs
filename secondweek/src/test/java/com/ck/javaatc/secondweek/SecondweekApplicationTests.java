4850d65ff217e208abc262837909f70c4081c6c9
coderCKK <chengkkaaii@163.com>
2021-05-16 23:20:31 +0800

    other

---
+++ secondweek/src/test/java/com/ck/javaatc/secondweek/SecondweekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck.javaatc.secondweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondweekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
