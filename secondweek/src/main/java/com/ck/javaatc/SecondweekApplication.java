9f0c9121f0645e83f8e08096a223c8436da9e3b0
coderCKK <chengkkaaii@163.com>
2021-05-16 23:17:46 +0800

    使用spring boot 编写一个简单服务器打开8081端口接收请求. OKHttpUtils.java 是使用OkHttp编写的发送请求的工具.

---
+++ secondweek/src/main/java/com/ck/javaatc/SecondweekApplication.java
@@ -0,0 +1,13 @@
+package com.ck.javaatc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondweekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondweekApplication.class, args);
+    }
+
+}
---
+++ secondweek/src/main/java/com/ck/javaatc/secondweek/util/OKHttpUtils.java
@@ -0,0 +1,27 @@
+package com.ck.javaatc.secondweek.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OKHttpUtils {
+    // 缓存客户端
+    private static OkHttpClient okClient = new OkHttpClient();
+
+    // 发送get请求, 返回body 字符串
+    public static String getAsString(String url) throws IOException {
+        Request request = new Request.Builder().url(url).build();
+        try(Response res = okClient.newCall(request).execute()){
+            return res.body().string();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String url = "http://localhost:8081";
+        String body = getAsString(url);
+        System.out.println("url: " + url + "; response:" + body);
+    }
+
+}
